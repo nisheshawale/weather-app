3128226c8b2ad5d02988ad5c96d241446412b809
nisheshawale <nisheshawale@gmail.com>
2023-06-15 22:49:12 +0545

    [feat]: 3D earth view loading with GeoJsonData

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Viewer, GeoJsonDataSource } from 'resium';
+import axios from 'axios';
 
-function App() {
+
+const WeatherMap = () => {
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/box/city?bbox=-74,40,-73,41,10&appid=03c27f6bdfbfbd05ce95cb05b778f7a6`
+        );
+
+        setWeatherData(response.data.list);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const createGeoJsonData = () => {
+    const geoJsonData = {
+      type: 'FeatureCollection',
+      features: weatherData.map((data) => {
+        if (!data.coord || typeof data.coord.lon !== 'number' || typeof data.coord.lat !== 'number') {
+          return null; // Skip invalid data points
+        }
+
+        return {
+          type: 'Feature',
+          properties: {
+            temperature: data.main.temp,
+            cloudCoverage: data.clouds.all,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [data.coord.lon, data.coord.lat],
+          },
+        };
+      }).filter((feature) => feature !== null), // Filter out null values
+    };
+
+    return geoJsonData;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Viewer full>
+      <GeoJsonDataSource data={createGeoJsonData()} />
+    </Viewer>
   );
-}
+};
 
-export default App;
+export default WeatherMap;

21cad3455181b968d70e7ab307552398b446b286
nisheshawale <nisheshawale@gmail.com>
2023-06-16 23:05:34 +0545

    [feat]: Coordinates of all US states added and visualized

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import { Viewer, GeoJsonDataSource } from 'resium';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Viewer, Entity, PolygonGraphics } from 'resium';
+import { jsonData } from './us-states';
+import * as Cesium from 'cesium';
 
+const MyMap = () => {
+  // const [jsonData, setJsonData] = useState(null);
 
-const WeatherMap = () => {
-  const [weatherData, setWeatherData] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('/home/nishesh/Documents/Personal/Projects/weather-app/weather-app/us-states.json');
+  //       console.log("The data")
+  //       console.log(response.json());
+  //       const data = await response.json();
+  //       setJsonData(data.features);
+  //     } catch (error) {
+  //       console.error('Error fetching JSON:', error);
+  //     }
+  //   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/box/city?bbox=-74,40,-73,41,10&appid=03c27f6bdfbfbd05ce95cb05b778f7a6`
-        );
+  //   fetchData();
+  // }, []);
 
-        setWeatherData(response.data.list);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
+  // if (!jsonData) {
+  //   return <div>Loading...</div>;
+  // }
 
-    fetchData();
-  }, []);
-
-  const createGeoJsonData = () => {
-    const geoJsonData = {
-      type: 'FeatureCollection',
-      features: weatherData.map((data) => {
-        if (!data.coord || typeof data.coord.lon !== 'number' || typeof data.coord.lat !== 'number') {
-          return null; // Skip invalid data points
-        }
-
-        return {
-          type: 'Feature',
-          properties: {
-            temperature: data.main.temp,
-            cloudCoverage: data.clouds.all,
-          },
-          geometry: {
-            type: 'Point',
-            coordinates: [data.coord.lon, data.coord.lat],
-          },
-        };
-      }).filter((feature) => feature !== null), // Filter out null values
-    };
-
-    return geoJsonData;
-  };
-
+  // console.log(typeof jsonData[0].geometry.coordinates[0].flat().map(obj => parseFloat(obj))[0]);
+  // console.log(Array.isArray(jsonData));
+  // console.log(jsonData[0].geometry.type === "Polygon");
+  // jsonData.forEach((eachJson) => {
+  //   if(eachJson.geometry.type === "Polygon") {
+  //     console.log("Aaku")
+  //   }
+  // })
+  
   return (
-    <Viewer full>
-      <GeoJsonDataSource data={createGeoJsonData()} />
+    <Viewer>
+      {jsonData.map((eachJson, index) => {
+        if (eachJson.geometry.type === "Polygon") {
+          return (
+            <Entity key={index}>
+              <PolygonGraphics
+                hierarchy={Cesium.Cartesian3.fromDegreesArray(eachJson.geometry.coordinates[0].flat())}
+                material={Cesium.Color.GREEN.withAlpha(0.5)}
+              />
+            </Entity>
+          );
+        } else {
+          return eachJson.geometry.coordinates[0].map((coordinate, id) => (
+            <Entity key={`${index}-${id}`}>
+              <PolygonGraphics
+                hierarchy={Cesium.Cartesian3.fromDegreesArray(coordinate.flat())}
+                material={Cesium.Color.GREEN.withAlpha(0.5)}
+              />
+            </Entity>
+      ))}
+      })}
     </Viewer>
+    // <Viewer>
+    //   {
+    //       <Entity>
+    //         <PolygonGraphics
+    //           hierarchy={Cesium.Cartesian3.fromDegreesArray(jsonData[0].geometry.coordinates[0].flat())}
+    //           material={Cesium.Color.RED.withAlpha(0.5)}
+    //         />
+    //       </Entity>
+    //   }
+    // </Viewer>
   );
 };
 
-export default WeatherMap;
+export default MyMap;
